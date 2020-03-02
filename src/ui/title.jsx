b73c3d3f292aacb1983196a44e0a9338ef59af20
Anastasiya Alekseeva <anastasiya.alekseeva@simbirsoft.com>
2020-03-02 11:40:57 +0400

    bugfix prettier

--- src/ui/title.jsx
+++ src/ui/title.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { bem } from "./bem";
-import "./title.scss";
+import React from 'react';
+import { bem } from './bem';
+import './title.scss';
 
-const cl = bem("title");
+const cl = bem('title');
 
 export const Title = () => <h1 className={cl()}>Hello World</h1>;
